6e4ac9ade133fb5987d050bbd15b40cd2244fc4f
Ruslan Porchinskiy <ruslan@Ruslans-MacBook-Pro.local>
2015-10-28 11:39:03 +0200

    columns

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def key_press(key):
 
 
 from docker import Client
-api = Client(base_url='unix://docker.sock', version='auto')
+api = Client(base_url='unix://var/run/docker.sock', version='auto')
 
 palette = [
     # ('body','black','light gray', 'standout'),
@@ -26,8 +26,13 @@ class MenuItem(urwid.WidgetWrap):
 
     def __init__(self, container):
         self.container = container
-        col = urwid.Columns([urwid.Text(self.container[u'Names'][0]), urwid.Text("testc dc ds")])
-        w = urwid.AttrMap(col, None ,'focustext')
+        cols = []
+        cols.append(urwid.Text(self.container[u'Names'][0]))
+        cols.append(urwid.Text((self.container[u'Status'])))
+        cols.append(('weight', 1, urwid.Text(self.container[u'Command'])))
+        cols.append(urwid.Text(self.container[u'Image']))
+        cols = urwid.Columns(cols)
+        w = urwid.AttrMap(cols, None ,'focustext')
         self.__super.__init__(w)
 
     def selectable (self):
@@ -41,6 +46,8 @@ class MenuItem(urwid.WidgetWrap):
 l = []
 
 containers = api.containers(filters={'status': 'running'})
+# print containers[0]
+# exit()
 for container in containers:
     l.append(MenuItem(container))
 
