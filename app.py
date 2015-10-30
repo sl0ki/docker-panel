b953a9fa7270e85f9bc15803e527ceb9b08a1fc0
sl0ki <ruslano.prv@gmail.com>
2015-10-30 19:01:08 +0200

    events

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+from twisted.internet import reactor, defer
+from threading import Thread
+from docker import Client
 from docker import Client
 import json
 import urwid
-from docker import Client
-from twisted.internet import pollreactor
-pollreactor.install()
-from twisted.internet import reactor, defer
-
 
 
 class ContainerListItem(urwid.WidgetWrap):
@@ -24,13 +22,13 @@ class ContainerListItem(urwid.WidgetWrap):
             ('weight', 10, urwid.Text(name, align='left', wrap='clip')),
             ('weight', 7, urwid.Text(status, align='left', wrap='clip')),
             ('weight', 12, urwid.Text(image, align='left', wrap='clip')),
-            ('weight', 12, urwid.Text(command , align='left', wrap='clip')),
+            ('weight', 12, urwid.Text(command, align='left', wrap='clip')),
         ]),
         cols = urwid.Columns(cols, dividechars=1)
-        w = urwid.AttrMap(cols, None ,'focustext')
+        w = urwid.AttrMap(cols, None, 'focustext')
         self.__super.__init__(w)
 
-    def selectable (self):
+    def selectable(self):
         return True
 
     def keypress(self, size, key):
@@ -55,20 +53,39 @@ class ContainerList(urwid.ListBox):
 
 class Ui:
 
-    frame = None
+    palette = [('focustext', 'light gray', 'dark cyan'), ('footer', 'light gray', 'dark cyan')]
 
     def __init__(self):
-        listbox = ContainerList()
-        self.frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'))
+
+        self.listbox = ContainerList()
+        self.frame = urwid.Frame(urwid.AttrWrap(self.listbox, 'body'))
+        self.render_footer()
+        self.loop = urwid.MainLoop(self.frame, self.palette, unhandled_input=self.key_press, event_loop=urwid.TwistedEventLoop(reactor=reactor)).run()
+
+    def render_footer(self):
+        footer_text = "r: run  s: stop  a: all s: search q: quit"
+        footer = urwid.AttrMap(urwid.Text(footer_text, align='left'), 'footer')
+        self.frame.footer = footer
 
     def key_press(self, key):
+        if key in ('u', 'U'):
+            self.update()
+
         if key in ('q', 'Q'):
             raise urwid.ExitMainLoop()
 
+    def update(self):
+        self.listbox.update()
+
+
+def event_watcher():
+        events = docker_api.events()
+        for raw in events:
+            evt = json.loads(raw)
+            print evt
+            ui.update()
 
 docker_api = Client(base_url='unix://var/run/docker.sock', version='auto')
-palette = [ ('focustext','light gray','dark cyan') ]
 ui = Ui()
-
-urwid.MainLoop(ui.frame, palette, unhandled_input=ui.key_press, event_loop=urwid.TwistedEventLoop(reactor=reactor)).run()
+Thread(target=event_watcher).start()
 reactor.run()
