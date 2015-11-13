460de8478f4cae602bf822f7918774fbd95b2413
sl0ki <ruslano.prv@gmail.com>
2015-11-13 11:58:59 +0200

    pypi build

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import os
 from setuptools import setup
 
-
 REQUIREMENTS = [
     'docker-py==1.5.0',
     'gevent==1.0.2',
@@ -12,25 +11,17 @@ REQUIREMENTS = [
 
 setup(
     name="docker-panel",
-    version="0.0.2",
+    version="0.0.6",
     author="sl0ki",
     author_email="ruslano.prv@gmail.com",
     description=("A graphical terminal control panel for Docker."),
     license="GPLv3",
     keywords="docker control panel container cli",
     url="https://github.com/sl0ki/docker-panel",
-    packages=['docker-panel'],
-    package_dir={'docker-panel': ''},
-    # classifiers=[
-    #     "Programming Language :: Python :: 2",
-    #     "Development Status :: 1 - Beta",
-    #     # "Topic :: Utilites :: Sound/Audio",
-    #     "License :: OSI Approved :: GNU General Public License (GPL)",
-    #     "Environment :: Console",
-    # ],
+    packages=['docker_panel'],
     entry_points={
         'console_scripts': [
-            'docker-panel=docker_panel:main',
+            'docker-panel=docker_panel.cli:main',
         ],
     },
     install_requires=REQUIREMENTS,
